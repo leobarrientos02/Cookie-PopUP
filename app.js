9a09936ae32161da343e884e1b260823f55a5854
Your Name <leobarrientos02@gmail.com>
2022-01-08 04:41:23 -0500

    gsap

--- app.js
+++ app.js
@@ -3,15 +3,46 @@
 //   opacity: 0,
 //   duration: 1,
 // });
-gsap.fromTo(
-  ".text",
+
+// gsap.fromTo(
+//   ".text",
+//   {
+//     opacity: 0,
+//     y: 20,
+//   },
+//   {
+//     y: 0,
+//     opacity: 1,
+//     duration: 1,
+//   }
+// );
+
+const tl = gsap.timeline({
+  defaults: {
+    duration: 0.75,
+  },
+});
+
+tl.fromTo(
+  ".cookie-container",
   {
-    opacity: 0,
-    y: 20,
+    scale: 0,
   },
   {
-    y: 0,
+    scale: 1,
+  }
+);
+
+tl.fromTo(
+  ".cookie",
+  {
+    opacity: 0,
+    x: -50,
+    rotation: "-45deg",
+  },
+  {
     opacity: 1,
-    duration: 1,
+    x: 0,
+    rotation: "0deg",
   }
 );
